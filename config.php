8f0d537c7dc12a89b05a3af50c7d50a212942c9d
Thai Tu <Thaitu@gmail.com>
2024-10-31 14:45:48 +0700

    gtet

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php /** The name of the database*/
+define( 'DB_NAME', 'database_name_here' );
+/** MySQL database username */
+define( 'DB_USER', 'username_here' );
+/** MySQL database password */
+define( 'DB_PASSWORD', 'password_here' );
+/** MySQL hostname */
+define( 'DB_HOST', 'localhost' );
+/** port number of DB */
+define( 'PORT', 3306);
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
